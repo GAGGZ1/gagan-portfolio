ee8188937b56cc7c4b8c0630a06e9a9a318b6b36
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-01-26 05:53:37 +0530

    fixed pdf file path

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -6,7 +6,7 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { pdfjs } from "react-pdf";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const Resume = () => {
   return (
